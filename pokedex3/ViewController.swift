2a67a146ef9bee8e8c15a0a8200d47fe62ab9209
Pelayo Mercado <pelayo@Pelayos-MacBook-Pro.local>
2017-06-04 17:40:28 -0400

    Initial Commit

---
+++ pokedex3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex3
+//
+//  Created by Pelayo Mercado on 6/4/17.
+//  Copyright © 2017 Pelayo Mercado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
